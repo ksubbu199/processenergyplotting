a6eb638e389c131f32a5c3a035b2c6312c6bfea0
Subrahamanyam Varma <ksubbu199@gmail.com>
2017-12-05 18:54:15 +0530

    Top regions added

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,95 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/home', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
+
+
+function getRandomColor() {
+           var letters = '0123456789ABCDEF'.split('');
+           var color = '#';
+           for (var i = 0; i < 6; i++ ) {
+               color += letters[Math.floor(Math.random() * 16)];
+           }
+           return color;
+        }
+function getChartData(region12)
+{
+  var yodata=[]
+  for(var i=0;i<region12.length;i++)
+  {
+    var proc ='Process '+i;
+    var atemp=[];
+    var temp={};
+    var ftemp={
+      label: [proc],
+      backgroundColor: getRandomColor(),
+      //hoverBackgroundColor: "#000000",
+      //hoverBorderColor: "#9966FF",
+      //hoverBorderWidth: 5,
+      //hoverRadius: 5
+    };
+    temp.x=region12[i].ProcNo;
+    temp.y=region12[i].Energy;
+    temp.r=30//region12[i].Energy;
+    atemp.push(temp);
+    ftemp.data=atemp;
+
+    yodata.push(ftemp);
+  }
+  return JSON.stringify(yodata);
+}
+
+router.get('/', function(req, res, next) {
+  var data = require("../data.json");
+  var regionNo=req.query.region;
+  var groupBy = function(xs, key) {
+    return xs.reduce(function(rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
+  var groupedByRegion=groupBy(data, 'RegionID');
+  function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        //return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        return a + b;
+    });
+  }
+
+  var sortput=[];
+  for (var key in groupedByRegion) {
+    if (groupedByRegion.hasOwnProperty(key)) {
+      groupedByRegion[key]= groupedByRegion[key].sort(function(a,b){
+        return b.Energy - a.Energy;
+        }
+      );//sortByKey(groupedByRegion[key],'Energy');
+      sortput.push({key:key,max:groupedByRegion[key][0].Energy});
+    }
+  }
+
+  sortput= sortput.sort(function(a,b){
+    return b.max - a.max;
+    }
+  );
+  console.log(sortput);
+  finalRegions=[];
+
+  for(var i=0;i<5;i++)
+  {
+    var key=sortput[i].key;
+    var tempP={data: getChartData(groupedByRegion[key]), key:key};
+    //finalRegions[key]='';
+    //tempP[key]=[];
+    finalRegions.push(tempP);
+  }
+  console.log(JSON.stringify(finalRegions));
+  res.render('plots', { title: 'Express' , regions: finalRegions});
+});
+
+
 router.get('/plot', function(req, res, next) {
   var data = require("../data.json");
   var regionNo=req.query.region;
